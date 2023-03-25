232f1505a321295cb132bc55c66cf37a6bc47451
EliasCode79 <eliasfranco2379@gmail.com>
2023-03-24 22:33:34 -0400

    docs :memo: The files have been sorted

---
+++ 1.python_for_beginner/02_operator.py
@@ -0,0 +1,23 @@
+# in python we have the following arithmetic operators
+
+#sum
+suma = 2+2 
+resta = 4-2
+multiplicacion = 2*5
+division = 10/2
+modulo = 9%4
+
+print(f'addition:{suma}, subtraction:{resta},  multiplication:{multiplicacion}, divison{division}, module:{modulo}')
+
+
+# operator assingt
+x = 10 # asignation
+print(f'asignation: {x}')
+x+= 2 # increase of 2
+print(f'increase of 2 {x}')
+x-=2 # decrease of 2
+print(f'decrease of 2 {x}')
+x*=2 # multiplied by 2
+print(f'multiplied by {x}')
+x/=2 # divided by 2
+print(f'divided by 2 {x}')
